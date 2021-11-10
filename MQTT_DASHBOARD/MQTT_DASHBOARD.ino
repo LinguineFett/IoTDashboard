de20768e1e2355fe56e2e2be890f80764c63313b
Nic <nicolasspagnolo64@gmail.com>
2021-11-10 13:32:30 -0500

    Added email

--- MQTT_DASHBOARD/MQTT_DASHBOARD.ino
+++ MQTT_DASHBOARD/MQTT_DASHBOARD.ino
@@ -1,8 +1,20 @@
+#include <ESP_Mail_Client.h>
+#include <ESP_Mail_FS.h>
+#include <SDK_Version_Common.h>
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "DHTesp.h"
+
 #define LED 0 // D3 on the Node MCU
 
+#define SMTP_HOST "smtp.gmail.com" //SMTP gmail host.
+#define SMTP_PORT 465 //SMTP gmail port.
+#define AUTHOR_EMAIL "example@gmail.com" //Sender email.
+#define AUTHOR_PASSWORD "password" //Sender email password.
+#define RECIPIENT_EMAIL "example@gmail.com" //Just set this as the same as the sender email.
+
+SMTPSession smtp;
 DHTesp dht;
 const char* ssid = "TP-Link_8856";
 const char* password = "87973365";
@@ -14,6 +26,39 @@ const char* mqtt_server = "192.168.0.104";
 WiFiClient vanieriot;
 PubSubClient client(vanieriot);
 
+void setup_email(){
+   smtp.callback(smtpCallback);
+   ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "ESP Test Email";
+  message.addRecipient("Admin", RECIPIENT_EMAIL);
+  
+  String textMsg = "The temperature is passed the threshold! Turn on LED?";
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
 
 void setup_wifi() {
   delay(10);
@@ -112,3 +157,31 @@ void loop() {
 
   delay(5000);
   }
+  
+  void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
+ }
